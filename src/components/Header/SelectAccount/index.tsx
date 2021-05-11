2d05010b8465355ed249029a902429d3c8880179
hongchen <hongchen@chainnet.tech>
2021-05-11 14:44:52 +0800

    feat: add header accountLists

--- src/components/Header/SelectAccount/index.tsx
+++ src/components/Header/SelectAccount/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { SelectAccountStyle } from "./style";
-import dropdownlogo from "../SelectAccount/icons/Drop down.svg"
+import { SelectAccountStyle, AccountLists, CurrentAccount } from "./style";
+import dropdown from "../SelectAccount/icons/Drop_down.svg"
+import selectAccount from "../SelectAccount/icons/selected_Account.svg"
 import { Modal, Radio } from "antd";
 import useAccountModel from "../../../hooks/useAccountModel";
 import { useAccountInfo } from "../../../hooks/useAccountInfo";
@@ -8,17 +9,63 @@ import {useApi} from "../../../hooks/useApi"
 function SelectAccount() {
   const accountModel = useAccountModel();
   const [AccountListModal, SetAccountListModal] = useState(false);
+  const [isShowAccountList, setIsShowAccountList] = useState(false)
+  const [isSelected, setIsSelected] = useState(false)
   const { currentAccount } = useAccountModel();
   const { accounts } = useAccountModel();
   const [value, setValue] = useState(currentAccount);
   const currentAddress = currentAccount?.address;
+  const currAddress = currentAddress?.substring(0, 5) + '...' + currentAddress?.substring(currentAddress.length - 5)
+  const list = [
+    {
+      name: 'Alex',
+      address: '5RkTK…c31Cr'
+    },
+    {
+      name: 'Pedro Amorim',
+      address: '5RkTK…c31Cr'
+    },
+    {
+      name: 'Kiandra Lowe',
+      address: '5RkTK…c31Cr'
+    },
+    {
+      name: 'Helena Brauer',
+      address: '5RkTK…c31Cr'
+    }
+  ]
+  function accountSelected(val: any) {
+    setIsSelected(true)
+    setValue(val)
+    setIsShowAccountList(!isShowAccountList)
+  }
   return (
     <SelectAccountStyle>
-      <div className={"avatar"}></div>
-      <div className={"current-account"}>{currentAccount?.name}</div>
-      <div className={"icon"}>
-          <img src={dropdownlogo} alt=""/>
-      </div>
+      <CurrentAccount className='currentAccount' onClick={()=>setIsShowAccountList(!isShowAccountList)}>
+        <div className={"avatar"}></div>
+        <div className={"current-account"}>{ value && value?.name ? value?.name : currentAccount?.name }</div>
+        <div className={"icon"}>
+            <img src={dropdown} alt=""/>
+        </div>
+      </CurrentAccount>
+      {
+        isShowAccountList && 
+        <div className='accountList'>
+          {list.map(function (item: any) {
+            return (
+              <AccountLists className="accountItem" onClick={()=>accountSelected(item)}>
+                <div className='selectImg'>
+                  { isSelected && item.name === value?.name ? <img src={selectAccount} alt='selected' /> : <div className='circle' /> }
+                </div>
+                <div className='accountMsg'>
+                  <div className='nameMsg'>{item.name}</div>
+                  <div className='adressMsg'>{item.address}</div>
+                </div>
+              </AccountLists>
+            );
+          })}
+        </div>
+      }
     </SelectAccountStyle>
   );
 }
--- src/components/Header/SelectAccount/style.tsx
+++ src/components/Header/SelectAccount/style.tsx
@@ -1,17 +1,30 @@
 import styled from "styled-components";
 
 export const SelectAccountStyle = styled.div`
-  padding-left: 20px;
-  padding-right: 20px;
+
+  position: relative;
+  width: 100%;
   margin-right: 24px;
+  .accountList {
+    position: absolute;
+    top: 46px;
+    background: #FFFFFF;
+    box-shadow: 6px 6px 8px 0 rgba(0,0,0,0.06);
+    border-radius: 8px;
+  }
+`
+
+export const CurrentAccount = styled.div`
+  padding: 5px 20px;
   display: flex;
   align-items: center;
   background: #F4F4F5;
   box-shadow: 3px 3px 8px 0 rgba(0,0,0,0.11), -3px -3px 8px 0 #FFFFFF;
   border-radius: 16px;
-  width: 100%;
   justify-content: center;
-  .avatar{
+  width: 100%;
+  height: 100%;
+  .avatar {
     margin-right: 8px;
   }
   .icon{
@@ -23,4 +36,45 @@ export const SelectAccountStyle = styled.div`
     font-weight: bold;
     white-space: nowrap;
   }
+`
+export const AccountLists = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 16px 24px 30px;
+  width: 190px;
+  cursor: pointer;
+  &:hover {
+    background: #F7F7F7;
+  }
+  &:hover .selectImg .circle{
+    border: 1px solid #F6C94A;
+  }
+  .selectImg {
+    .circle {
+      width: 16px;
+      height: 16px;
+      background: #FFFFFF;
+      border: 1px solid #8E8E8E;
+      border-radius: 50%;
+      // &:hover {
+      //   border: 1px solid #F6C94A;
+      // }
+    }
+  }
+  .accountMsg {
+    margin-left: 16px;
+    font-size: 14px;
+    .nameMsg {
+      font-family: NotoSansSC-Medium;
+      color: #282828;
+      font-weight: 500;
+      margin-bottom: 2px;
+    }
+    .adressMsg {
+      font-family: NotoSansSC-Regular;
+      font-weight: 400;
+      color: #8E8E8E;
+    }
+  }
+
 `
