df841321351f8c3e518f99487c3171753b56305a
limiu <845217637@qq.com>
2021-05-17 15:58:17 +0800

    feat: add history

--- src/page/History/History.tsx
+++ src/page/History/History.tsx
@@ -10,6 +10,7 @@ import RequestID from "../../components/RequestID";
 import ModalFooter from "../../components/ModalFooter";
 import ProcessingModal from "../../components/ProcessingModal/ProcessingModal";
 import StatusModal from "../../components/StatusModal";
+import axios from "axios";
 
 interface HistoryRow {
     id: number;
@@ -21,22 +22,18 @@ interface HistoryRow {
     status: "process" | "completed" | "cancelled";
 }
 
-enum HistoryTab {
-    Issue,
-    Redeem
-}
-
 function History() {
     const {currentAccount} = useAccountModel();
     const {t} = useTranslation()
     const [page, setPage] = useState(0);
     const [currentTable, setCurrentTable] = useState("issue")
-    const [IssueprocessingModalVisbible,SetIssueprocessingModalVisbible] = useState(false)
-    const [IssuesuccessModalVisible,setIssueSuccessModalVisible] = useState(false)
-    const [RedeemSuccessModalVisible,setRedeemSuccessModalVisible] = useState(false)
-    const [IssuefailModalVisible,setIssueFailModalVisible] = useState(false)
-    const [RedeemfailModalVisible,setRedeemfailModalVisible] = useState(false)
-    const [RedeemProcessingModalVisible,setRedeemProcessingModalVisible] = useState(false)
+    const [IssueprocessingModalVisbible, SetIssueprocessingModalVisbible] = useState(false)
+    const [IssuesuccessModalVisible, setIssueSuccessModalVisible] = useState(false)
+    const [RedeemSuccessModalVisible, setRedeemSuccessModalVisible] = useState(false)
+    const [IssuefailModalVisible, setIssueFailModalVisible] = useState(false)
+    const [RedeemfailModalVisible, setRedeemfailModalVisible] = useState(false)
+    const [RedeemProcessingModalVisible, setRedeemProcessingModalVisible] = useState(false)
+    const [IssueData,setIssueData] = useState([])
     const Issuecolumns = [
         {
             title: '更新时间',
@@ -45,8 +42,8 @@ function History() {
         },
         {
             title: '数量（XBTC）',
-            dataIndex: 'number',
-            key: 'number',
+            dataIndex: 'btcAmount',
+            key: 'btcAmount',
         },
         {
             title: 'BTC交易',
@@ -60,17 +57,20 @@ function History() {
         },
         {
             title: 'Chainx块高',
-            dataIndex: 'number',
-            key: 'number',
+            dataIndex: 'openTime',
+            key: 'openTime',
         },
         {
             title: '状态',
-            key: 'action',
+            key: 'status',
             render: (text: any, record: any) => (
                 <Space size="middle">
-                    {record.status === "进行中" && <div className={"processing"} onClick={()=> SetIssueprocessingModalVisbible(true)}>{record.status}</div>}
-                    {record.status === "失败" && <div onClick={()=> setIssueFailModalVisible(true)} className={"fail"}>{record.status}</div>}
-                    {record.status === "成功" && <div onClick={()=> setIssueSuccessModalVisible(true)}>{record.status}</div>}
+                    {record.status === "processing" && <div className={"processing"}
+                                                     onClick={() => SetIssueprocessingModalVisbible(true)}>{record.status}</div>}
+                    {record.status === "失败" &&
+                    <div onClick={() => setIssueFailModalVisible(true)} className={"fail"}>{record.status}</div>}
+                    {record.status === "成功" &&
+                    <div onClick={() => setIssueSuccessModalVisible(true)}>{record.status}</div>}
                 </Space>
             ),
         },
@@ -107,56 +107,14 @@ function History() {
             key: 'action',
             render: (text: any, record: any) => (
                 <Space size="middle">
-                    {record.status === "进行中" && <div className={"processing"} onClick={()=> setRedeemProcessingModalVisible(true)}>{record.status}</div>}
-                    {record.status === "失败" && <div onClick={()=> setRedeemfailModalVisible(true)} className={"fail"}>{record.status}</div>}
-                    {record.status === "成功" && <div onClick={()=> setRedeemSuccessModalVisible(true)}>{record.status}</div>}
+                    {record.status === "进行中" && <div className={"processing"}
+                                                     onClick={() => setRedeemProcessingModalVisible(true)}>{record.status}</div>}
+                    {record.status === "失败" &&
+                    <div onClick={() => setRedeemfailModalVisible(true)} className={"fail"}>{record.status}</div>}
+                    {record.status === "成功" &&
+                    <div onClick={() => setRedeemSuccessModalVisible(true)}>{record.status}</div>}
                 </Space>
             ),
-        },
-    ];
-
-    const IssueData = [
-        {
-            key: '1',
-            time: '2021-04-16 18:20',
-            number: 0.00001,
-            address: <span className='hash'>1b2978...fd247ac</span>,
-            status: '成功'
-        },
-        {
-            key: '2',
-            time: '2021-04-16 18:20',
-            number: 0.00001,
-            address: <span className='hash'>1b2978...fd247ac</span>,
-            status: '进行中'
-        },
-        {
-            key: '3',
-            time: '2021-04-16 18:20',
-            number: 0.00001,
-            address: <span className='hash'>1b2978...fd247ac</span>,
-            status: '失败'
-        },
-        {
-            key: '4',
-            time: '2021-04-16 18:20',
-            number: 0.00001,
-            address: <span className='hash'>1b2978...fd247ac</span>,
-            status: '成功'
-        },
-        {
-            key: '5',
-            time: '2021-04-16 18:20',
-            number: 0.00001,
-            address: <span className='hash'>1b2978...fd247ac</span>,
-            status: '成功'
-        },
-        {
-            key: '6',
-            time: '2021-04-16 18:20',
-            number: 0.00001,
-            address: <span className='hash'>1b2978...fd247ac</span>,
-            status: '成功'
         },
     ];
     const RedeemData = [
@@ -203,6 +161,12 @@ function History() {
             status: '成功'
         },
     ];
+    useEffect(() => {
+        axios.get('https://api-btc.chainx.org/xbridge/issue_requests?page=0&pageSize=5&requester=5UzcC9WB4VAXRs3TqebcsbfLNKmpengHQU4tHpPs43udpEpW').then((res) => {
+            console.log(res)
+            setIssueData(res.data.items)
+        })
+    }, [])
     return (
         <HistoryStyle>
             <FunctionSwitchButton>
@@ -218,16 +182,24 @@ function History() {
                 </ul>
             </FunctionSwitchButton>
             <TableStyle>
-                {currentTable === "issue" ? 
-                <Table columns={Issuecolumns} dataSource={IssueData} pagination={{pageSize:5,defaultPageSize:5}} /> :
-                <Table columns={Redeemcolumns} dataSource={RedeemData} pagination={{pageSize:5,defaultPageSize:5}} />}
+                {currentTable === "issue" ?
+                    <Table columns={Issuecolumns} dataSource={IssueData}
+                           pagination={{pageSize: 5, defaultPageSize: 5}}/> :
+                    <Table columns={Redeemcolumns} dataSource={RedeemData}
+                           pagination={{pageSize: 5, defaultPageSize: 5}}/>}
             </TableStyle>
-            <ProcessingModal visible={IssueprocessingModalVisbible} cancle={()=> SetIssueprocessingModalVisbible(false)}/>
-            <StatusModal visible={IssuesuccessModalVisible} cancle={()=> setIssueSuccessModalVisible(false)} type={"issue-success"}/>
-            <StatusModal visible={RedeemSuccessModalVisible} cancle={()=> setRedeemSuccessModalVisible(false)} type={"redeem-success"}/>
-            <StatusModal visible={IssuefailModalVisible} cancle={()=> setIssueFailModalVisible(false)} type={"issue-fail"}/>
-            <StatusModal visible={RedeemfailModalVisible} cancle={()=> setRedeemfailModalVisible(false)} type={"redeem-fail"}/>
-            <StatusModal visible={RedeemProcessingModalVisible} cancle={()=> setRedeemProcessingModalVisible(false)} type={"redeem-processing"}/>
+            <ProcessingModal visible={IssueprocessingModalVisbible}
+                             cancle={() => SetIssueprocessingModalVisbible(false)}/>
+            <StatusModal visible={IssuesuccessModalVisible} cancle={() => setIssueSuccessModalVisible(false)}
+                         type={"issue-success"}/>
+            <StatusModal visible={RedeemSuccessModalVisible} cancle={() => setRedeemSuccessModalVisible(false)}
+                         type={"redeem-success"}/>
+            <StatusModal visible={IssuefailModalVisible} cancle={() => setIssueFailModalVisible(false)}
+                         type={"issue-fail"}/>
+            <StatusModal visible={RedeemfailModalVisible} cancle={() => setRedeemfailModalVisible(false)}
+                         type={"redeem-fail"}/>
+            <StatusModal visible={RedeemProcessingModalVisible} cancle={() => setRedeemProcessingModalVisible(false)}
+                         type={"redeem-processing"}/>
         </HistoryStyle>
     )
 }
